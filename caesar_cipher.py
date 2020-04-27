ee63716ceda72b5a551b6335610e2f073182821e
Crispr-Fries <danat19@protonmail.com>
2020-04-26 19:02:04 -0500

    Version 1 of the cipher.  The cipher is working correctly.  The encryption shifts properly and wraps from z to a.  The encryption also ignores special characters and numbers.

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,15 +1,35 @@
+#take input message and shift key from user and store it
+
+#take input message and shift key from user and store it
+
 print('What do you want to encrypt?')
 msg = input()
-key = int(input('What key do you want to use(enter a number)  '))
+shiftkey = int(input('What shiftkey do you want to use(enter a number)  '))
+
+
+#convert string to list and iterate over list to convert letters to unicode
 
 msg_orig = list(msg)
-print(msg_orig)
 
-cypher = [ord(letter)+key for letter in msg_orig]
+unicode_list = [ord(letter) for letter in msg_orig]
 
+encrypt = []
 
-print(cypher)
-print(ord('t'))
-
-msg_encrypt = [chr(x) for x in cypher]
-print(msg_encrypt)
+#if the characters are a-z then shift them based on the key provided, the bounds below are a-z in upper and lower case. The second
+for letter in unicode_list:
+  shifted = letter + shiftkey
+  if (letter >=97) & (letter <=122) or (letter >=65) & (letter <= 90):
+    #second if cycles value back to a if the shift pushes past z. 
+    if shifted > 122:
+      letter = 96 + (shifted - 122)
+      encrypt.append(chr(letter))
+    elif shifted > 90 and shifted < 97:
+      letter = 64 + (shifted - 90)
+      encrypt.append(chr(letter))
+    else:
+      letter += shiftkey
+      encrypt.append(chr(letter))
+  else:
+    encrypt.append(chr(letter))
+#convert encrypted list to string
+print ("".join(encrypt))
